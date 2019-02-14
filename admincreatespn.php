76a4dfa1739c9286864a795f98bdc411f0debbb7
TwinSickles <sheste19@icloud.com>
2019-02-14 16:57:51 -0500

    ADDED THIS

---
+++ admincreatespn.php
@@ -0,0 +1,32 @@
+<html>
+<body> 
+<form method="post" action="output.php"> 
+<table bgcolor="#C4C4C4" align="center" width="380" border="0">   
+<tr> 
+<td  align="center"colspan="2"><font color="#0000FF" size="5">Create Sponsor Account</font></td>   
+</tr>   
+<tr> 
+<td width="312"></td> 
+<td width="172"> </td>   
+</tr>   
+<tr> 
+<td>Enter User's Email </td> 
+<td><input type="email" name="email" /></td>   
+</tr>   
+<tr> 
+<td>Enter User's Password </td> 
+<td><input type="password" name="password"  /></td>   
+</tr>   
+<tr> 
+<td>Enter User's Username </td> 
+<td><input type="username" name="username"  /></td>   
+</tr>   
+<tr> 
+<td>Enter User's Comapny Name </td> 
+<td><input type="company_name" name="company_name"  /></td>   
+</tr>
+<td align="center" colspan="2"><input type="submit" value="Submit" name="submit" /></td> 
+</table>
+</form> 
+</body> 
+</html>
