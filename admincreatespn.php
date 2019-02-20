946cc8b3cf6869dfdbab8bd741085e25872c5412
Logan <lczarne@clemson.edu>
2019-02-20 15:24:04 +0000

    added some styling

--- admincreatespn.php
+++ admincreatespn.php
@@ -1,5 +1,23 @@
 <html>
+<head>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
 <body> 
+	<header>
+	  <div class="container">
+	    <div id="branding">
+	      <h1><span class="highlight">Drewp:</span> <u>D</u>river <u>REW</u>ards <u>P</u>rogram</h1>
+	    </div>
+	    <nav>
+	      <ul>
+	        <li><a href="/">Home</a></li>
+	        <li><a href="about.html">About</a></li>
+	        <li><a href="stories.html">[Stories]</a></li>
+	        <li><a href="">Login/Signup</a></li>
+	    </ul>
+	    </nav>
+	  </div>
+	</header>
 <form method="post" action="sponsoroutput.php"> 
 <table bgcolor="#C4C4C4" align="center" width="380" border="0">   
 <tr> 
