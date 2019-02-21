d873db745e605d8159eb3ddf24fe7cdb15bc8b06
TwinSickles <sheste19@icloud.com>
2019-02-21 00:40:41 -0500

    ADDED THIS

---
+++ admincreatesdvr.php
@@ -0,0 +1,49 @@
+<html>
+<body> 
+<form method="post" action="driveroutput.php"> 
+<table bgcolor="#C4C4C4" align="center" width="380" border="0">   
+<tr> 
+<td  align="center"colspan="2"><font color="#0000FF" size="5">Create Driver Account</font></td>   
+</tr>   
+<tr> 
+<td width="312"></td> 
+<td width="172"> </td>   
+</tr>   
+<tr> 
+<td>Enter User's Email </td> 
+<td><input type="email" name="email" /></td>   
+</tr>   
+<tr> 
+<td>Enter User's Password </td> 
+<td><input type="password" name="password"  /></td>   
+</tr>   
+<tr> 
+<td>Enter User's Username </td> 
+<td><input type="username" name="username"  /></td>   
+</tr>   
+<tr> 
+<td>Enter User's First Name </td> 
+<td><input type="firstname" name="firstname"  /></td>   
+</tr>
+<tr> 
+<td>Enter User's Last Name </td> 
+<td><input type="lastname" name="lastname"  /></td>   
+</tr>
+<td>Enter User's Street Address </td> 
+<td><input type="street_address" name="street_address"  /></td>   
+</tr>
+<td>Enter User's Country </td> 
+<td><input type="country" name="country"  /></td>   
+</tr>
+<td>Enter User's Postal Code </td> 
+<td><input type="postal_code" name="postal_code"  /></td>   
+</tr>
+</tr>
+<td>Enter User's Sponsor ID </td> 
+<td><input type="sponsor_id" name="sponsor_id"  /></td>   
+</tr>
+<td align="center" colspan="2"><input type="submit" value="Submit" name="submit" /></td> 
+</table>
+</form> 
+</body> 
+</html>
