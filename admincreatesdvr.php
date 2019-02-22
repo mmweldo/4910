10444cefe6b6e4d6eb54296e9f546ced2667f9d2
Logan <lczarne@clemson.edu>
2019-02-22 13:51:31 +0000

    updating master to reflect what was on instance

--- admincreatesdvr.php
+++ admincreatesdvr.php
@@ -1,5 +1,24 @@
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
 <body> 
+
+	<header>
+	  <div class="container">
+	    <div id="branding">
+	      <h1><span class="highlight">Drewp:</span> <u>D</u>river <u>REW</u>ards <u>P</u>rogram</h1>
+	    </div>
+	    <nav>
+	      <ul>
+	        <li><a href="/">Home</a></li>
+	        <li><a href="about.html">About</a></li>
+	        <li><a href="stories.html">[Stories]</a></li>
+	        <li><a href="">Login/Signup</a></li>
+	    </ul>
+	    </nav>
+	  </div>
+	</header>
 <form method="post" action="driveroutput.php"> 
 <table bgcolor="#C4C4C4" align="center" width="380" border="0">   
 <tr> 
@@ -45,5 +64,9 @@
 <td align="center" colspan="2"><input type="submit" value="Submit" name="submit" /></td> 
 </table>
 </form> 
+	<footer>
+      		<p>Drewp, Copyright &copy; 2019</p>
+    	</footer>
+
 </body> 
 </html>
