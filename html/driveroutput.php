e0c3b8fcf5eb99e843fbdf3de13ddd61763926df
mmweldo <mmweldo@clemson.edu>
2019-02-24 11:56:36 -0500

    Connection tokens were wrong
    made it towards RDS instead of local

--- html/driveroutput.php
+++ html/driveroutput.php
@@ -14,12 +14,12 @@ $password = "";
 $dbname = "test";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 
-//$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
+$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
 
 // Create connection
-//$conn = mysqli_connect($endpoint, "master", "group4910", "website");
+$conn = mysqli_connect($endpoint, "master", "group4910", "website");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
