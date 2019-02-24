50eadb3e961abbd086b7ff0ef16e1dfd5c846704
Logan <lczarne@clemson.edu>
2019-02-24 16:42:16 +0000

    Changed the table from drivers to driver_list because for RFC#1 that is where points should be taken from

--- html/pointsTotal.php
+++ html/pointsTotal.php
@@ -4,7 +4,7 @@
 	#$conn = mysqli_connect("127.0.0.1", "root", "", "test");
 	$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
 	$conn = mysqli_connect($endpoint, "master", "group4910", "website");
-	$query = "select total_points from drivers where username = '".$_POST['username']."';";
+	$query = "select total_points from driver_list where driver_username = '".$_POST['username']."';";
 	$result = mysqli_query($conn, $query);
     
 	while($row=mysqli_fetch_row($result)){
