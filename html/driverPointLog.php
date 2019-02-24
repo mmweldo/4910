095770bb1efabae224e9b07c4a544ee131decd46
Logan <lczarne@clemson.edu>
2019-02-24 16:38:26 +0000

    Added css to output

--- html/driverPointLog.php
+++ html/driverPointLog.php
@@ -1,5 +1,6 @@
 <?php 
-
+	echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.css\"><header><div class=\"container\"><div id=\"branding\"><h1><span class=\"highlight\">Drewp:</span> <u>D</u>river <u>REW</u>ards <u>P</u>rogram</h1></div><nav><ul><li><a href=\"/\">Home</a></li><li><a href=\"about.html\">About</a></li><li><a href=\"stories.html\">[Stories]</a></li><li><a href=\"\">Login/Signup</a></li></ul></nav></div></header>";
+	echo "<center>";
 
 	#$conn = mysqli_connect("127.0.0.1", "root", "", "test");
 	$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
@@ -19,7 +20,6 @@ $query = "select points_history.date_created, points_history.point_amount, drive
 		echo " ".$row[1]." ";
 		echo "</p>";
 	}
+	echo "</center>";
 	mysqli_close($conn);
-
-
 ?>
