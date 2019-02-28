54bec55b8a4bf308dfcc4b2fc37d2574b921424b
TwinSickles <sheste19@icloud.com>
2019-02-28 17:45:55 -0500

    added updates

--- html/admincreatesdvr.php
+++ html/admincreatesdvr.php
@@ -20,7 +20,7 @@
 	  </div>
 	</header>
 	<form method="post" action="driveroutput.php"> 
-		<table bgcolor="#C4C4C4" align="center" width="380" border="0">   
+		<table bgcolor="#C4C4C4" align="center" width="400" border="0">   
 		<tr> 
 			<td  align="center"colspan="2"><font color="#0000FF" size="5">Create Driver Account</font></td>   
 			</tr>   
@@ -30,29 +30,29 @@
 		</tr>   
 		<tr> 
 			<td>Enter User's Email </td> 
-			<td><input type="email" name="email" required /></td>   
+			<td><input type="email" name="email" required title="Email"/></td>   
 		</tr>   
 			<tr> 
 			<td>Enter User's Password </td> 
-			<td><input type="password" name="password" required /></td>   
+			<td><input type="password" name="password" required title="Password"/></td>   
 		</tr>   
 		<tr> 
 			<td>Enter User's Username </td> 
-			<td><input type="text" name="username" required /></td>   
+			<td><input type="text" name="username" required title="Username"/></td>   
 		</tr>   
 		<tr> 
 			<td>Enter User's First Name </td> 
-			<td><input type="text" name="firstname" required /></td>   
+			<td><input type="text" name="firstname" required title="First Name"/></td>   
 		</tr>
 		<tr> 
 			<td>Enter User's Last Name </td> 
-			<td><input type="text" name="lastname" required /></td>   
+			<td><input type="text" name="lastname" required title="Last Name"/></td>   
 		</tr>
 			<td>Enter User's Street Address </td> 
-			<td><input type="text" name="street_address" required /></td>   
+			<td><input type="text" name="street_address" required title="Street Address"/></td>   
 		</tr>
 			<td>Enter User's Country </td> 
-			<td><input type="text" name="country" required /></td>   
+			<td><input type="text" name="country" required title="Country"/></td>   
 		</tr>
 			<td>Enter User's Postal Code </td> 
 			<td><input type="text" pattern="[0-9]{5}" name="postal_code" title="Five digit zip code" required /></td>   
