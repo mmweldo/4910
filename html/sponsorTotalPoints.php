ce1e8cb9aba0c3bf68146d8c2a70fe2d8b9f9754
mmweldo <mmweldo@clemson.edu>
2019-02-28 17:09:05 -0500

    conn change

--- html/sponsorTotalPoints.php
+++ html/sponsorTotalPoints.php
@@ -1,8 +1,9 @@
 <?php 
 	echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.css\"><header><div class=\"container\"><div id=\"branding\"><h1><span class=\"highlight\">Drewp:</span> <u>D</u>river <u>REW</u>ards <u>P</u>rogram</h1></div><nav><ul><li><a href=\"/\">Home</a></li><li><a href=\"about.php\">About</a></li><li><a href=\"stories.php\">[Stories]</a></li><li><a href=\"login.html\">Login/Signup</a></li></ul></nav></div></header>";
 	echo "<center>";
-
-	$conn = mysqli_connect("127.0.0.1", "root", "", "test");
+	$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
+	$conn = mysqli_connect($endpoint, "master", "group4910", "website");
+	#$conn = mysqli_connect("127.0.0.1", "root", "", "test");
 	$query2 = "SELECT driver_list.driver_username, driver_list.current_points from sponsors join driver_list on driver_list.sponsor_id = sponsors.user_id where sponsors.company_name ='".$_POST['username']."';";
 	$result2 = mysqli_query($conn, $query2);
 	if (!$result2) {
