1ed714631e4b8ddd7a5c3df6781c1829d4339f8d
mmweldo <mmweldo@clemson.edu>
2019-03-07 17:27:45 -0500

    fixed connection issues

--- html/adminupdatesadm.php
+++ html/adminupdatesadm.php
@@ -19,13 +19,16 @@
 	  </div>
 	</header>
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test";
+#$servername = "localhost";
+#$username = "root";
+#$password = "";
+#$dbname = "test";
 
+$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($endpoint, "master", "group4910", "website");
+// Create connection
+#$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
