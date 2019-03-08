4c5a7eda4bbaba55bb562dfc128c3d236c5d4cf5
mmweldo <mmweldo@clemson.edu>
2019-03-08 15:44:06 -0500

    Update driverPerformance.php

--- html/driverPerformance.php
+++ html/driverPerformance.php
@@ -1,4 +1,17 @@
 <?php 
+	session_start();
+	if(!isset($_SESSION['username'])){
+		echo "Error: Please log in first!";
+		echo "<script>setTimeout(\"location.href = '../login.html?NOT-LOGGED-IN';\", 3000);</script>";
+		exit();
+	}
+	//Check if appropriate user (admin) for page
+	if($_SESSION['user_type'] != "driver"){
+		echo "Error: User not a driver!";
+		echo "<script>setTimeout(\"location.href = '../index.php?NOT-DRIVER';\", 3000);</script>";
+		exit();
+	}
+
 	echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.css\"><header><div class=\"container\"><div id=\"branding\"><h1><span class=\"highlight\">Drewp:</span> <u>D</u>river <u>REW</u>ards <u>P</u>rogram</h1></div><nav><ul><li><a href=\"/\">Home</a></li><li><a href=\"about.php\">About</a></li><li><a href=\"stories.php\">[Stories]</a></li><li><a href=\"login.html\">Login/Signup</a></li></ul></nav></div></header>";
 	echo "<center>";
 	$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
