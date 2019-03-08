cd263d3b07cb02bb2cbae3e8a37a3120a6e255bf
mmweldo <mmweldo@clemson.edu>
2019-03-08 16:21:21 -0500

    Update viewsponsors.php

--- html/viewsponsors.php
+++ html/viewsponsors.php
@@ -1,4 +1,16 @@
 <?php  
+	session_start();
+	if(!isset($_SESSION['user_id'])){
+		echo "Error: User not logged in! Redirecting...";
+		echo "<script>setTimeout(\"location.href = '../index.php?NOT-LOGGED-IN';\", 3000);</script>";
+		exit();
+	}
+	if($_SESSION['user_type'] != "admin"){
+		echo "Error: User doesn't have permission to be here! Redirecting...";
+		echo "<script>setTimeout(\"location.href = '../index.php?NOT-ADMIN';\", 3000);</script>";
+		exit();
+	}
+
 	echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"./css/style.css\"><header><div class=\"container\"><div id=\"branding\"><h1><span class=\"highlight\">Drewp:</span> <u>D</u>river <u>REW</u>ards <u>P</u>rogram</h1></div><nav><ul><li><a href=\"/\">Home</a></li><li><a href=\"about.php\">About</a></li><li><a href=\"stories.php\">[Stories]</a></li><li><a href=\"login.html\">Login/Signup</a></li></ul></nav></div></header>";
 	#$conn = mysqli_connect("127.0.0.1", "root", "", "test");
 	$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
@@ -9,7 +21,7 @@
 	
 	if(!$result){
 		echo "Error: Error finding sponsors";
-		echo "<script>setTimeout(\"location.href = '../viewsponsors.html?CANT-FIND-SPONSOR';\", 3000);</script>";
+		echo "<script>setTimeout(\"location.href = '../viewsponsorshtml.php?CANT-FIND-SPONSOR';\", 3000);</script>";
 		exit();
 	}
 
@@ -17,7 +29,7 @@
 
 	if($resultCheck < 1){
 		echo "Error: Sponsor not found! Redirecting...";
-		echo "<script>setTimeout(\"location.href = '../viewsponsors.html?CANT-FIND-SPONSOR';\", 3000);</script>";
+		echo "<script>setTimeout(\"location.href = '../viewsponsorshtml.php?CANT-FIND-SPONSOR';\", 3000);</script>";
 		exit();
 	}
 
