398385b1dc988708dbca543307b7ab093c3aeece
mmweldo <mmweldo@clemson.edu>
2019-03-08 15:49:01 -0500

    Update and rename listofdrivers.html to listofdrivershtml.php

--- html/listofdrivershtml.php
+++ html/listofdrivershtml.php
@@ -1,3 +1,18 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['username'])){
+		echo "Error: Please log in first!";
+		echo "<script>setTimeout(\"location.href = '../login.html?NOT-LOGGED-IN';\", 3000);</script>";
+		exit();
+	}
+	//Check if appropriate user (admin) for page
+	if($_SESSION['user_type'] != "admin"){
+		echo "Error: User not an admin!";
+		echo "<script>setTimeout(\"location.href = '../index.php?NOT-ADMIN';\", 3000);</script>";
+		exit();
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
