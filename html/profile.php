5fd79abe9898522e6ba028671db2a9334e468dc3
TwinSickles <sheste19@icloud.com>
2019-03-11 13:44:36 -0400

    added profile updates

--- html/profile.php
+++ html/profile.php
@@ -1,33 +1,7 @@
-<?php
-	session_start();
-	if(!isset($_SESSION['user_id'])){
-		echo "Error: User not logged in! You need to be logged in to access profiles. Redirecting...";
-		echo "<script>setTimeout(\"location.href = '../index.php?NOT-LOGGED-IN';\", 3000);</script>";
-		exit();
-	}
-?>
-<html>
-<head>
-	<link rel="stylesheet" type="text/css" href="./css/style.css">
-</head>
-<body> 
-	<header>
-	  <div class="container">
-	    <div id="branding">
-	      <h1><span class="highlight">Drewp:</span> <u>D</u>river <u>REW</u>ards <u>P</u>rogram</h1>
-	    </div>
-	    <nav>
-	      <ul>
-	        <li><a href="/">Home</a></li>
-	        <li><a href="about.php">About</a></li>
-	        <li><a href="stories.php">[Stories]</a></li>
-	        <li><a href="login.html">Login/Signup</a></li>
-	    </ul>
-	    </nav>
-	  </div>
-	</header><link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
 <!--
 User Profile Sidebar by @keenthemes
@@ -36,35 +10,34 @@ Licensed under MIT
 -->
 
 <?php
-#$servername = "localhost";
-#$username = "root";
-#$password = "";
-#$dbname = "test";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
 
-$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
 // Create connection
-$conn = mysqli_connect($endpoint, "master", "group4910", "website");	
-	
-// Create connection
-#$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?> 
 
-<div class="container">
     <div class="row profile">
-		<div class="col-md-3">
+		<div class="col-md-10">
+            <div class="profile-content">
+			   Some user related content goes here...
+            </div>
+		</div>
+		<div class="col-md-2">
 			<div class="profile-sidebar">
 				<!-- SIDEBAR USERPIC -->
 				<div class="profile-userpic">
 					<?php
-						session_start();
-						$sql = "select profile_img from drivers where username = '".$_SESSION['username']."';";
-						$result = mysqli_query($conn, $sql);
-						$row = mysqli_fetch_row($result);
-						echo '<img src="'.$row[0].'">'; #class="img-circle" alt="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" style="width:125px;height:125px;">';
+							$sql = "select profile_img from sponsors where user_id = 5";
+							$result = mysqli_query($conn, $sql);
+							$row = mysqli_fetch_row($result);
+							echo '<img src='.$row[0].'class="img-circle" alt="Profile Image" style="width:125px;height:125px;">';
 					?>
 				</div>
 				<!-- END SIDEBAR USERPIC -->
@@ -72,16 +45,14 @@ if ($conn->connect_error) {
 				<div class="profile-usertitle">
 					<div class="profile-usertitle-name">
 						<?php
-							session_start();
-							#$sql = "select username from users where user_id = '".$_POST['user_id']."';";
-							#$result = mysqli_query($conn, $sql);
-							#$row = mysqli_fetch_row($result);
-							#echo $row[0]."<br>";
-							echo $_SESSION['username'];
+							$sql = "select username from users where id = 5";
+							$result = mysqli_query($conn, $sql);
+							$row = mysqli_fetch_row($result);
+							echo $row[0]."<br>";
 						?>
 					</div>
 					<div class="profile-usertitle-job">
-						User-Job-Type
+						Administrator
 					</div>
 				</div>
 				<!-- END SIDEBAR USER TITLE -->
@@ -119,13 +90,7 @@ if ($conn->connect_error) {
 				<!-- END MENU -->
 			</div>
 		</div>
-		<div class="col-md-9">
-            <div class="profile-content">
-			   Some user related content goes here...
-            </div>
-		</div>
 	</div>
-</div>
 <br>
 <br>
 
