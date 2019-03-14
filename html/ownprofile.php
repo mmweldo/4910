3628234c3f818ec033ec533270e111553404e005
mmweldo <mmweldo@clemson.edu>
2019-03-14 17:06:31 -0400

    Update ownprofile.php

--- html/ownprofile.php
+++ html/ownprofile.php
@@ -97,13 +97,17 @@ if ($conn->connect_error) {
 					<button class="tablinks" onclick="openCity(event, 'Points')"><p><span class="glyphicon glyphicon-ok"></span> Tasks</p></button>
 					<button class="tablinks" onclick="openCity(event, 'Drivers')"><p><span class="glyphicon glyphicon-flag"></span> Help</p></button>
 					<?php
+						
+					
 						session_start();
-						$sql = "select count(*) as total from applications where sponsor_id =".$_SESSION['user_id'];
-						$result = mysqli_query($conn, $sql);
-						$row = mysqli_fetch_row($result);
+						
 						if(!$_SESSION['username']) exit();
 						if($_SESSION['user_type'] == "sponsor" || $_SESSION['user_type'] == "driver"){
-							$query = 
+							$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
+							$conn = mysqli_connect($endpoint, "master", "group4910", "website");
+							$sql = "select count(*) as total from applications where sponsor_id =".$_SESSION['user_id']; 
+							$result = mysqli_query($conn, $sql);
+							$row = mysqli_fetch_row($result);
 							echo '<button class="tablinks" onclick="openCity(event, \'Applications\')"><p><span class="glyphicon glyphicon-user"></span> Applications';
 							echo $row[0];
 							echo '</p></button>';
