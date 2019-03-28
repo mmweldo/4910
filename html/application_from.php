f337b500573d8ef2c51b98ff6d58c36cb80054f0
mmweldo <mmweldo@clemson.edu>
2019-03-28 14:52:02 -0400

    Update application_from.php

--- html/application_from.php
+++ html/application_from.php
@@ -14,7 +14,7 @@
 	$conn = mysqli_connect($endpoint, "master", "group4910", "website");
 	#$conn = mysqli_connect("127.0.0.1", "root", "", "test");
 
-	echo "SELECT user_id FROM drivers WHERE username = '".$_POST['driver_username']."';";	
+	$query = "SELECT user_id FROM drivers WHERE username = '".$_POST['driver_username']."';";	
 	$result = mysqli_query($conn, $query);
 	if(!$result){
 		echo "Error: Driver Couldn't be found or accepted...";
@@ -22,6 +22,7 @@
 		exit();
 	}
 	$row = mysql_fetch_row($result);
+	echo $row[0];
 
 	echo "UPDATE applications SET status = '".$_POST['status']."' WHERE driver_id = ".$row[0].";";
 	$result = mysqli_query($conn, $query);
