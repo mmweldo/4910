c33de407df16d8f7d5e4f7c7c42b6c6769e0bae7
mmweldo <mmweldo@clemson.edu>
2019-03-29 11:18:00 -0400

    Update overview.php

--- html/overview.php
+++ html/overview.php
@@ -43,7 +43,7 @@ session_start();
         [11,  5.3],
         [12,  6.6],
       ]);
-	data.sort({column: 0, desc: true}, {column: 1, desc: true});
+	data.sort({column: 0, desc: true}, {column: 1, desc: false});
 
       var options = {
         chart: {
