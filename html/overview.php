07da73d72653b0ad2995f05a0ef25d02887f3806
mmweldo <mmweldo@clemson.edu>
2019-03-29 11:23:17 -0400

    Update overview.php

--- html/overview.php
+++ html/overview.php
@@ -18,52 +18,7 @@ session_start();
 ?>
 
 <head>
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['line']});
-      google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
-	
-      var data = new google.visualization.DataTable();
-      data.addColumn('number', 'Month');
-      data.addColumn('number', 'Current Points');
-
-      data.addRows([
-        [1,  37.8],
-        [2,  30.9],
-        [13,  25.4],
-        [4,  11.7],
-        [20,  11.9],
-        [21,   8.8],
-        [31,   7.6],
-        [31,  12.3],
-        [31,  16.9],
-        [10, 12.8],
-        [11,  5.3],
-        [12,  6.6],
-      ]);
-	data.sort({column: 0, desc: true}, {column: 1, desc: false});
-
-      var options = {
-        chart: {
-          title: 'Current Points Over Time',
-          subtitle: 'Shown by Month'
-        },
-        width: 900,
-        height: 500,
-        axes: {
-          x: {
-            0: {side: 'top'}
-          }
-        }
-      };
-
-      var chart = new google.charts.Line(document.getElementById('line_top_x'));
-
-      chart.draw(data, google.charts.Line.convertOptions(options));
-    }
-  </script>
 </head>
 <body>
   <div id="line_top_x"></div>
@@ -112,3 +67,49 @@ session_start();
 </html>
 </body>
 </html>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+	
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Month');
+      data.addColumn('number', 'Current Points');
+
+      data.addRows([
+        [1,  37.8],
+        [2,  30.9],
+        [13,  25.4],
+        [4,  11.7],
+        [20,  11.9],
+        [21,   8.8],
+        [31,   7.6],
+        [31,  12.3],
+        [31,  16.9],
+        [10, 12.8],
+        [11,  5.3],
+        [12,  6.6],
+      ]);
+	data.sort({column: 0, desc: true}, {column: 1, desc: false});
+
+      var options = {
+        chart: {
+          title: 'Current Points Over Time',
+          subtitle: 'Shown by Month'
+        },
+        width: 900,
+        height: 500,
+        axes: {
+          x: {
+            0: {side: 'top'}
+          }
+        }
+      };
+
+      var chart = new google.charts.Line(document.getElementById('line_top_x'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+  </script>
