f51dd864a9661d7820af285e55fd3f419c1bdb74
mmweldo <mmweldo@clemson.edu>
2019-03-29 10:40:57 -0400

    Update overview.php

--- html/overview.php
+++ html/overview.php
@@ -91,7 +91,25 @@ session_start();
 	$result = mysqli_query($conn, $sql);
 	$row = mysqli_fetch_row($result); ?>
 	<h3>Total Earned Points: <?php echo $row[0]."<br>"?></h3>
-	<?php $conn->close();?>
+	<?php 
+		$sql = "SELECT current_points, total_spent, total_points, company_name FROM (driver_list join drivers on driver_id = user_id) join sponsors on driver_list.sponsor_id = sponsors.user_id WHERE driver_id = ".$_SESSION['user_id']." and user_id = ".$_SESSION['user_id'].";";
+	
+	$result = mysqli_query($conn, $sql);
+	if(!result){
+		echo "didn't work!";
+	}
+
+	else while($rows=mysqli_fetch_row($result)){
+		$counter = 0;
+		if($counter == 0) echo '<h3>Total Spent Points: '.$rows[1].'<br></h3>';
+		echo '<h3>For Company: '.$rows[3].'</h3><br>';
+		echo '<h3>Total Current Points: '.$rows[0].'<br></h3>';
+		echo '<h3>Total Earned Points:'.$rows[2].'<br></h3>';
+		$counter = $counter + 1;
+	}
+	
+	
+	$conn->close();?>
 </html>
 </body>
 </html>
