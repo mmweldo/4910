0cd077b548b9f218070c881916b4988a24ba191a
mmweldo <mmweldo@clemson.edu>
2019-04-04 08:44:09 -0400

    Update driverheader.php

--- html/driverheader.php
+++ html/driverheader.php
@@ -16,15 +16,16 @@
         session_start();
         if(isset($_SESSION['username'])) echo '
           <form position="relative";  style="width=5%; float:right;" action="logout.php" method="POST">
-	  <button id="myButton" name="submit" type="submit" class="btn btn-danger">
-	  <a href="logout.php">Log Out</a>
-	  </button></form><style> .btn-danger{position:relative; right:20px; top: 20px;} </style>
+	  <button name="submit" type="submit" class="btn btn-danger">Log Out</button></form><style> .btn-danger{position:relative; right:20px; top: 20px;} </style>
           <script type="text/javascript">
             document.getElementById("myButton").onclick = function () {
               location.href = "https://52.55.244.84/logout.php";
             };
           </script>'; 
           ?>
+	  <!-- <button id="myButton" name="submit" type="submit" class="btn btn-danger">
+	  <a href="logout.php">Log Out</a>
+	  </button> -->
       <header>
       <?php
         if(isset($_SESSION['username'])){ 
