ebcf5ad83f6aa76120a17ae27af44d78aaff79da
mmweldo <mmweldo@clemson.edu>
2019-04-04 16:17:35 -0400

    Update sponsorviewcatalog.php

--- html/sponsorviewcatalog.php
+++ html/sponsorviewcatalog.php
@@ -49,7 +49,7 @@
 	$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
 	$conn = mysqli_connect($endpoint, "master", "group4910", "website");
 
-	echo "SELECT title, pic, link, price FROM products WHERE sponsor_id = ".$_SESSION['user_id'].";";
+	#echo "SELECT title, pic, link, price FROM products WHERE sponsor_id = ".$_SESSION['user_id'].";";
 	$sql = "SELECT title, pic, link, price FROM products WHERE sponsor_id = ".$_SESSION['user_id'].";";
 	
 	$result = mysqli_query($conn, $sql);
