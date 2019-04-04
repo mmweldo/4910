9161adcfbc023eb89ec22a87c80614d0fe7845b8
TwinSickles <sheste19@icloud.com>
2019-04-04 13:07:29 -0400

    Added admin view profile page

--- html/adminviewprofile.php
+++ html/adminviewprofile.php
@@ -2,8 +2,8 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link rel="stylesheet" type="text/css" href="/css/profileStyle.css">
-		<link rel="stylesheet" type="text/css" href="/css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/profileStyle.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 <!------ Include the above in your HEAD tag ---------->
 
@@ -27,6 +27,10 @@ Licensed under MIT
 	
 	$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
 	$conn = mysqli_connect($endpoint, "master", "group4910", "website");
+	
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
 }
 ?> 
 
