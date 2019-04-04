c7699af652a1d56beb0fec8da8c292d49844a62a
mmweldo <mmweldo@clemson.edu>
2019-04-04 15:07:59 -0400

    Update ebayfetch.php

--- html/ebayfetch.php
+++ html/ebayfetch.php
@@ -1,3 +1,16 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <meta name="description" content="Affordable and professional web design">
+  <meta name="keywords" content="web design, affordable web design, professional web design">
+  <meta name="author" content="Brad Traversy">
+  <title>Drewp | Stories</title>
+  <link rel="stylesheet" href="./css/style.css">
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
 
 <?php 
   session_start();
