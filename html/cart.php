fd141fb71b2eb1a0440f17d159b70101b3e2f716
Mitch <mmweldo@clemson.edu>
2019-04-12 18:48:21 -0400

    -

--- html/cart.php
+++ html/cart.php
@@ -42,6 +42,7 @@
 		
 		//Check if an existing cart item was added to cart
 		$sql = "SELECT title, amount, price FROM cart WHERE driver_id = ".$_SESSION['user_id']." AND title = '".$_POST['title']."';";
+		echo $sql;
 		
 		$result = mysqli_query($conn, $sql);
 		$in_cart_already = "false";
