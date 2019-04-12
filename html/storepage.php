aca2a0e9aefe9f492b3ac9367e61ea3da4cd1123
Mitch <mmweldo@clemson.edu>
2019-04-12 19:59:52 -0400

    -

--- html/storepage.php
+++ html/storepage.php
@@ -102,7 +102,7 @@
               $item_img = $row[2];//'http://placehold.it/700x400';
               $item_link = $row[3];//'#';
               $item_price = (double)$row[4] * $row[6];//'250';
-              $item_name = $row[7];//'item one';
+              $item_name = $row[0];//'item one';
               $item_rating = '<div class="card-footer">
                       <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
                     </div>';
