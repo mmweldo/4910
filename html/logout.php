bc4f390801f299017df43679f5f62ad85ee71b32
Mitch <mmweldo@clemson.edu>
2019-04-12 18:32:46 -0400

    adding redirect to logout

--- html/logout.php
+++ html/logout.php
@@ -3,7 +3,7 @@
 		session_start();
 		session_unset();
 		session_destroy();
-		header("Location:../index.php");
+		echo "<script>setTimeout(\"location.href = '../index.php?LOGGED-OUT;\",100);</script>"
 		exit();
 	}
 	
