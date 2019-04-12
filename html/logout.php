6039fd8e175685092639e68101de86d8a5bd7164
Mitch <mmweldo@clemson.edu>
2019-04-12 18:39:24 -0400

    -

--- html/logout.php
+++ html/logout.php
@@ -3,7 +3,7 @@
 		session_start();
 		session_unset();
 		session_destroy();
-		echo "<script>setTimeout(\"location.href = '../index.php?LOGGED-OUT;\",100);</script>";
+		echo "<script>window.top.location.href=\"http://52.55.244.84/\s"</script>";//setTimeout(\"location.href = '../index.php?LOGGED-OUT;\",100);
 		exit();
 	}
 	
