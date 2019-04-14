554817e24c5cad1e859bfb8c56d1ec0afde78c5f
Mitch <mmweldo@clemson.edu>
2019-04-14 10:00:13 -0400

    -

--- html/checkout.php
+++ html/checkout.php
@@ -118,7 +118,7 @@
             $sql = "INSERT INTO products_bought (order_id, sponsor_id, driver_id, price, point_cost, title, amount) VALUES (".$row[0].",".$_POST['sponsor_id'].",".$_SESSION['user_id'].",".$_POST[].","","","");";
 
             //Remove from cart the things that were added--------------------------------------------------------
-            $sql = "DELETE FROM cart WHERE driver_id = ".$_SESSION['user_id']." AND title = '".$_POST['title']."';";
+            $sql = "DELETE FROM cart WHERE driver_id = ".$_SESSION['user_id'].' AND title = \''.$_POST['title'].'\';';
             /*$result = mysqli_query($conn, $sql);
             if(!$result){
                  echo "<br>error<br>";
