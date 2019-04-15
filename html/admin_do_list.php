effc18de5a4ed9c5ad7b54536aebce851b81d231
Mitch <mmweldo@clemson.edu>
2019-04-15 08:27:58 -0400

    -

--- html/admin_do_list.php
+++ html/admin_do_list.php
@@ -16,6 +16,26 @@
 ?>
 
 <body><center>
+    <h2>Admin - View Sponsor Driver List</h2>
+    <form class="driverlist-form" method="post" action="driverlist.php">
+        <p>Company Name | Sort</p>
+        <input type="text" name="company_username" placeholder="company_username">
+        <select name="order">
+            <option value="drivers.firstname ASC">Fname Asc</option>
+            <option value="drivers.firstname DESC">Fname Desc</option>
+            <option value="drivers.lastname ASC">Lname Asc</option>
+            <option value="drivers.lastname DESC">Lname Desc</option>
+            <option value="drivers.username ASC">Username Asc</option>
+            <option value="drivers.username DESC">Username Desc</option>
+            <option value="drivers.total_points ASC">Total Pts Asc</option>
+            <option value="drivers.total_points DESC">Total Pts Desc</option>
+            <option value="drivers.current_points ASC">Curr Pts Asc</option>
+            <option value="drivers.current_points DESC">Curr Pts Desc</option>
+            <option value="users.date_created ASC">Date Created Asc</option>
+            <option value="users.date_created DESC">Date Created Desc</option>
+        </select>
+        <button type="submit" name="submit">Submit</button>
+    </form>
     <!------------------------------------------------------------------------------------------------>
     <h2>Admin - Remove Driver from Sponsor List</h2>
     <form method="post" action="admrmdrlist.php">
