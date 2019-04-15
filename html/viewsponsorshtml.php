f2a676a53cecb004f65dd83f51736fd61dca01d1
Mitch <mmweldo@clemson.edu>
2019-04-15 16:37:30 -0400

    -

--- html/viewsponsorshtml.php
+++ html/viewsponsorshtml.php
@@ -68,6 +68,36 @@
 			</select>
 			<button type="submit" name="submit">Submit</button>
 		</form>';
+	if($_SESSION['user_type'] == "admin" || $_SESSION['user_type'] == "sponsor"){
+		echo '<h2>View Driver List</h2>
+			<form class="driverlist-form" method="post" action="driverlist.php">
+			<p>Company Name | Sort</p>';
+		
+		if($_SESSION['user_type'] == "admin"){
+			echo'<input type="text" name="company_username" placeholder="company_username">';
+		}
+		else if ($_SESSION['user_type'] == "sponsor"){
+			echo'<p>'.$_SESSION['username'].'</p><input type="hidden" name="company_username" placeholder="company_username" value="'.$_SESSION['username'].'">';
+		}else {
+			echo "You shouldn't be here!";
+		}
+
+		echo '<select name="order">
+			<option value="drivers.firstname ASC">Fname Asc</option>
+			<option value="drivers.firstname DESC">Fname Desc</option>
+			<option value="drivers.lastname ASC">Lname Asc</option>
+			<option value="drivers.lastname DESC">Lname Desc</option>
+			<option value="drivers.username ASC">Username Asc</option>
+			<option value="drivers.username DESC">Username Desc</option>
+			<option value="drivers.total_points ASC">Total Pts Asc</option>
+			<option value="drivers.total_points DESC">Total Pts Desc</option>
+			<option value="drivers.current_points ASC">Curr Pts Asc</option>
+			<option value="drivers.current_points DESC">Curr Pts Desc</option>
+			<option value="users.date_created ASC">Date Created Asc</option>
+			<option value="users.date_created DESC">Date Created Desc</option>
+		</select>
+		<button type="submit" name="submit">Submit</button>';
+	}
 ?>
 		</center>
 	</div>
