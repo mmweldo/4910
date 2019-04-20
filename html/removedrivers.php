05f8ba7063b2fc09daec041463130b9ec508f46e
Mitch <mmweldo@clemson.edu>
2019-04-20 19:26:36 -0400

    -

--- html/removedrivers.php
+++ html/removedrivers.php
@@ -5,54 +5,90 @@
 		echo "<script>setTimeout(\"location.href = '../index.php?NOT-LOGGED-IN';\", 3000);</script>";
 		exit();
 	}
-
-	if($_SESSION['user_type'] != "sponsor"){
-		echo "Error: User doesn't have permission to be here! Redirecting...";
-		echo "<script>setTimeout(\"location.href = '../index.php?NOT-SPONSOR';\", 3000);</script>";
-		exit();
-	}
-
-	include 'sponsorheader.php';
-	echo '<a style="position:relative; left:0px; float:left;" href="/viewsponsorshtml.php"><button class="btn btn-success btn-sm">Sponsor Management Page</button></a>';
-
-	#$conn = mysqli_connect("127.0.0.1", "root", "", "test");
+	
 	$endpoint = "db-group-instance.cp7roxttzlg6.us-east-1.rds.amazonaws.com";
 	$conn = mysqli_connect($endpoint, "master", "group4910", "website");
 	
-	$query = "SELECT driver_id FROM driver_list WHERE sponsor_id = (SELECT user_id FROM sponsors WHERE company_name = '".$_POST['company_name']."') AND driver_username = '".$_POST['username']."';";
+	if($_SESSION['user_type'] == "sponsor"){
+		/*echo "Error: User doesn't have permission to be here! Redirecting...";
+		echo "<script>setTimeout(\"location.href = '../index.php?NOT-SPONSOR';\", 3000);</script>";
+		exit();*/
+		include 'sponsorheader.php';
+		echo '<a style="position:relative; left:0px; float:left;" href="/viewsponsorshtml.php"><button class="btn btn-success btn-sm">Sponsor Management Page</button></a>';
+		
+		$query = "SELECT driver_id FROM driver_list WHERE sponsor_id = (SELECT user_id FROM sponsors WHERE company_name = '".$_POST['company_name']."') AND driver_username = '".$_POST['username']."';";
 
-	$result = mysqli_query($conn, $query);
-	$row = mysqli_fetch_row($result);
-	if(!$result){
-		echo "[1] Error: Driver-Sponsor pair not found! Redirecting...";
-		#echo "<script>setTimeout(\"location.href = '../removedrivers.html?NONEXISTANT-PAIR';\", 3000);</script>";
-		exit();
+		$result = mysqli_query($conn, $query);
+		$row = mysqli_fetch_row($result);
+		if(!$result){
+			echo "[1] Error: Driver-Sponsor pair not found! Redirecting...";
+			#echo "<script>setTimeout(\"location.href = '../removedrivers.html?NONEXISTANT-PAIR';\", 3000);</script>";
+			exit();
+		}
+
+		$resultCheck = mysqli_num_rows($result);
+
+		if($resultCheck < 1){
+			echo "[2] Error: Sponsor not found! Redirecting...";
+			echo "<script>setTimeout(\"location.href = '../viewsponsorshtml.php?NONEXISTANT-PAIR';\", 3000);</script>";
+			exit();
+		}
+
+		$query = 'DELETE FROM driver_list WHERE driver_id = '.$row[0].' AND sponsor_id = '.$_SESSION['user_id'].';';
+		$result = mysqli_query($conn, $query);
+
+		$query = 'DELETE FROM applications WHERE driver_id = '.$row[0].' AND sponsor_id = '.$_SESSION['user_id'].';';
+		$result = mysqli_query($conn, $query);
+		if(!$result){
+			echo "[3] Error: Couldn't remove driver from list...";
+			echo "<script>setTimeout(\"location.href = '../viewsponsorshtml.php?COULDNT-DELETE';\", 3000);</script>";
+			exit();		
+		}
+		$query = 'DELETE FROM applications WHERE driver_id = '.$row[0].' AND sponsor_id = '.$_SESSION['user_id'].';';
+		$result = mysqli_query($conn, $query);
+
+		echo "<center>";
+		echo "<h3>Removed driver ".$_POST['username']."</h3>";
+		echo "</center>";
+		
+	}else if($_SESSION['user_type'] == "driver"){
+		include 'driverheader.php';
+		echo '<a style="position:relative; left:0px; float:left;" href="/viewsponsorshtml.php"><button class="btn 	btn-success btn-sm">Sponsor Management Page</button></a>';
+		
+		$query = "SELECT sponsor_id FROM driver_list WHERE driver_id = ".$_SESSION['user_id']." AND sponsor_id = ".$_POST['user_id'].";";
+
+		$result = mysqli_query($conn, $query);
+		$row = mysqli_fetch_row($result);
+		if(!$result){
+			echo "[1] Error: Driver-Sponsor pair not found! Redirecting...";
+			#echo "<script>setTimeout(\"location.href = '../removedrivers.html?NONEXISTANT-PAIR';\", 3000);</script>";
+			exit();
+		}
+
+		$resultCheck = mysqli_num_rows($result);
+
+		if($resultCheck < 1){
+			echo "[2] Error: Sponsor not found! Redirecting...";
+			echo "<script>setTimeout(\"location.href = '../viewsponsorshtml.php?NONEXISTANT-PAIR';\", 3000);</script>";
+			exit();
+		}
+
+		$query = 'DELETE FROM driver_list WHERE driver_id = '.$_SESSION['user_id'].' AND sponsor_id = '.$_POST['user_id'].';';
+		$result = mysqli_query($conn, $query);
+
+		$query = 'DELETE FROM applications WHERE driver_id = '.$_SESSION['user_id'].' AND sponsor_id = '.$_POST['user_id'].';';
+		$result = mysqli_query($conn, $query);
+		if(!$result){
+			echo "[3] Error: Couldn't remove driver from list...";
+			echo "<script>setTimeout(\"location.href = '../viewsponsorshtml.php?COULDNT-DELETE';\", 3000);</script>";
+			exit();		
+		}
+		$query = 'DELETE FROM applications WHERE driver_id = '.$_SESSION['user_id'].' AND sponsor_id = '.$_POST['user_id'].';';
+		$result = mysqli_query($conn, $query);
+
+		echo "<center>";
+		echo "<h3>Removed Sponsor!</h3>";
+		echo "</center>";
 	}
-
-	$resultCheck = mysqli_num_rows($result);
-
-	if($resultCheck < 1){
-		echo "[2] Error: Sponsor not found! Redirecting...";
-		echo "<script>setTimeout(\"location.href = '../viewsponsorshtml.php?NONEXISTANT-PAIR';\", 3000);</script>";
-		exit();
-	}
-
-	$query = 'DELETE FROM driver_list WHERE driver_id = '.$row[0].' AND sponsor_id = '.$_SESSION['user_id'].';';
-	$result = mysqli_query($conn, $query);
-
-	$query = 'DELETE FROM applications WHERE driver_id = '.$row[0].' AND sponsor_id = '.$_SESSION['user_id'].';';
-	$result = mysqli_query($conn, $query);
-	if(!$result){
-		echo "[3] Error: Couldn't remove driver from list...";
-		echo "<script>setTimeout(\"location.href = '../viewsponsorshtml.php?COULDNT-DELETE';\", 3000);</script>";
-		exit();		
-	}
-	$query = 'DELETE FROM applications WHERE driver_id = '.$row[0].' AND sponsor_id = '.$_SESSION['user_id'].';';
-	$result = mysqli_query($conn, $query);
-	
-	echo "<center>";
-	echo "<h3>Removed driver ".$_POST['username']."</h3>";
-	echo "</center>";
-
 	mysqli_close($conn);
 ?>
