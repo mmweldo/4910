5cecabf0b494fe0273186d9029e638683aa56d43
Mitch <mmweldo@clemson.edu>
2019-04-20 15:23:16 -0400

    -

--- html/driverpurchases.php
+++ html/driverpurchases.php
@@ -52,10 +52,10 @@
             $format = "Y-m-d H:i:s";
 
             $current_date = date("Y-m-d H:i:s");
-            echo $current_date;
+            echo 'CurrentDate: '.$current_date;
             
             $purchase_date = new DateTime($row[0]);
-            echo $purchase_date;
+            echo 'PurchaseDate: '.$purchase_date;
             //$purchase_date_plus_three = date_add($purchase_date->format("Y-m-d H:i:s"), 'P3d');
             //$purchase_date_plus_three = date_add($purchase_date, date_interval_create_from_date_string('3 days'));
            
@@ -63,8 +63,8 @@
             //$date = date_create_from_format($format, $row[0]);
             $date = strtotime($row[0]);
 
-            echo date($format, $date);
-            echo $current_date->diff($date);
+            echo 'PurchaseDate2: '.date($format, $date);
+            echo 'DifferenceDates: '.$current_date->diff($date);
 
             //echo $current_date->diff($purchase_date);
             //echo $current_date->diff($purchase_date_plus_three);
